5b53b13aa18e74e8d40266fdf713fbdb5bfac8eb
unknown <Romangergisevich@gmail.com>
2024-01-09 23:02:03 +0300

    second commit

--- index.js
+++ index.js
@@ -1,73 +1,60 @@
-const TelegramApi = require('node-telegram-bot-api');
-const {gameOptions, newGameOptions} = require('./options')
-const tgToken = '6564322584:AAHwufY6Tf_wloJhpa3ctdkkaWxI0vDtYaE';
+const TelegramApi = require("node-telegram-bot-api");
+const tgToken = "6564322584:AAHwufY6Tf_wloJhpa3ctdkkaWxI0vDtYaE";
+const axios = require("axios");
 
-const chats = {};
+const bot = new TelegramApi(tgToken, { polling: true });
 
-const bot = new TelegramApi(tgToken, {polling: true});
+bot.setMyCommands([
+  { command: "/start", description: "Начало работы" },
+  { command: "/info", description: "Информация о моих возможностях" },
+  { command: "/weather", description: "Вывести погоду" },
+]);
 
-// bot.setMyCommands([
-//     {command: '/start', description: 'Начало работы'},
-//     {command: '/info', description: 'Информация'},
-// ])
-
-// bot.on('message', async msg => {
-//     const text = msg.text;
-//     const chatId = msg.chat.id;
-
-//     if(text == '/start') {
-//         await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/e65/38d/e6538d88-ed55-39d9-a67f-ad97feea9c01/3.webp')
-//         await bot.sendMessage(chatId, `Hail And Kill Brother!`);
-//     }
-//     if(text == '/info') {
-//         await bot.sendMessage(chatId, `Hail to the ${msg.from.first_name}`)
-//     }
-// })
-
-const newGame = async (chatId) => {
-    await bot.sendMessage(chatId, 'Я загадаю цифру от 0 до 9, а ты попытайся отгадать.');
-    const randomNumber = Math.floor(Math.random() * 10);
-    chats[chatId] = randomNumber;
-    await bot.sendMessage(chatId, 'Отгадывай', gameOptions);
-}
-
-const start = () => {
-    bot.setMyCommands([
-        {command: '/start', description: 'Начало работы'},
-        {command: '/info', description: 'Информация'},
-        {command: '/game', description: 'игра в "отгадай цифру"'},
-    ])
-    
-    bot.on('message', async msg => {
-        const text = msg.text;
-        const chatId = msg.chat.id;
-    
-        if(text == '/start') {
-            await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/e65/38d/e6538d88-ed55-39d9-a67f-ad97feea9c01/3.webp')
-            return bot.sendMessage(chatId, `Hail And Kill, Brother!`);
-        }
-        if(text == '/info') {
-            return bot.sendMessage(chatId, `Hail to the ${msg.from.first_name}`)
-        }
-        if( text == '/game') {
-            return newGame(chatId);
-        }else {
-            return bot.sendMessage(chatId, 'Не понял тебя')
-        }
-    })
-
-    bot.on('callback_query', async msg => {
-        const data = msg.data;
-        const chatId = msg.message.chat.id;
-        if (data == '/newGame') {
-            return newGame(chatId);
-        }
-        if (data == chats[chatId]) {
-            return await bot.sendMessage(chatId, `Поздравляю, ты отгадал. Загаданной цифрой была ${chats[chatId]}`, newGameOptions);
-        } else {
-            return await bot.sendMessage(chatId, `Не угадал. Была загадана цифра ${chats[chatId]}`, newGameOptions);
-        }
-    })
-}
-
-start();
+bot.on("message", async (msg) => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
+  if (msg.location) {
+    let latitude = msg.location.latitude;
+    let longitude = msg.location.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=b14e69a74786d8e6191dd31323bd1cc0`;
+    const response = await axios.get(url);
+    return bot.sendMessage(
+      chatId,
+      `На данный момент погода в ${response.data.name} выглядит так. Температура воздуха: ${Math.floor(
+        response.data.main.temp - 273.15
+      )} ℃. Ощущается как ${Math.floor(
+        response.data.main.feels_like - 273.15
+      )} ℃. Скорость ветра: ${response.data.wind.speed} м/с. Влажность: ${
+        response.data.main.humidity
+      }%.`
+    );
+  }
+  if (text == "/start") {
+    return bot.sendMessage(chatId, `Hail And Kill, Brother!`);
+  }
+  if (text == "/info") {
+    return bot.sendMessage(
+      chatId,
+      `Hail to you, ${msg.from.first_name}. Что бы узнать погоду в своём местоположении, вышли свою геолокацию, либо введи команду /weather, тогда я выведу погоду в Бресте.`
+    );
+  }
+  if (text == "/weather") {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${52.0975}&lon=${23.6877}&appid=b14e69a74786d8e6191dd31323bd1cc0`;
+    const response = await axios.get(url);
+    return bot.sendMessage(
+      chatId,
+      `На данный момент погода в Бресте выглядит так. температура воздуха: ${Math.floor(
+        response.data.main.temp - 273.15
+      )} ℃. Ощущается как ${Math.floor(
+        response.data.main.feels_like - 273.15
+      )} ℃. Скорость ветра: ${response.data.wind.speed} м/с. Влажность: ${
+        response.data.main.humidity
+      }%.`
+    );
+  } else {
+    return bot.sendMessage(
+      chatId,
+      "Я не понимаю тебя. Попробуй ввести команду /info, она поможет ознакомиться с моими возможностями."
+    );
+  }
+});
