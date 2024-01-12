62903f68858a1cf4b8d1dd5b9d792fdc9a589e6f
unknown <Romangergisevich@gmail.com>
2024-01-12 04:26:36 +0300

    some shit

--- index.js
+++ index.js
@@ -1,37 +1,13 @@
 require("dotenv").config();
 const TelegramApi = require("node-telegram-bot-api");
 const tgToken = process.env.TG_TOKEN;
+const { defaultKeyboard, inlineKeyboardPanel } = require("./buttons");
 const axios = require("axios");
-
 const bot = new TelegramApi(tgToken, { polling: true });
-
-// const gameOptions = {
-//   reply_markup: JSON.stringify({
-//     inline_keyboard: [
-//       [
-//         { text: "1", callback_data: "1" },
-//         { text: "2", callback_data: "2" },
-//         { text: "3", callback_data: "3" },
-//       ],
-//       [
-//         { text: "4", callback_data: "4" },
-//         { text: "5", callback_data: "5" },
-//         { text: "6", callback_data: "6" },
-//       ],
-//       [
-//         { text: "7", callback_data: "7" },
-//         { text: "8", callback_data: "8" },
-//         { text: "9", callback_data: "9" },
-//       ],
-//       [{ text: "0", callback_data: "0" }],
-//     ],
-//   }),
-// };
 
 bot.setMyCommands([
   { command: "/start", description: "Начало работы" },
   { command: "/info", description: "Информация о моих возможностях" },
-  { command: "/weather", description: "Вывести погоду" },
 ]);
 
 bot.on("message", async (msg) => {
@@ -56,31 +32,35 @@ bot.on("message", async (msg) => {
     );
   }
   if (text == "/start") {
-    return bot.sendMessage(chatId, `Hail And Kill, Brother!`);
+    return bot.sendMessage(
+      chatId,
+      `Привет, ${msg.from.first_name}!`,
+      inlineKeyboardPanel
+    );
   }
   if (text == "/info") {
     return bot.sendMessage(
       chatId,
-      `Hail to you, ${msg.from.first_name}. Что бы узнать погоду в своём местоположении, вышли свою геолокацию, либо введи команду /weather, тогда я выведу погоду в Бресте.`
+      `Hail to you, ${msg.from.first_name}. Что бы узнать погоду в своём местоположении, вышли свою геолокацию, либо введи команду /weather, тогда я выведу погоду в Бресте.`,
+      inlineKeyboardPanel
     );
   }
-  if (text == "/weather") {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${52.0975}&lon=${23.6877}&appid=b14e69a74786d8e6191dd31323bd1cc0`;
-    const response = await axios.get(url);
-    return bot.sendMessage(
-      chatId,
-      `На данный момент погода в Бресте выглядит так. температура воздуха: ${Math.floor(
-        response.data.main.temp - 273.15
-      )} ℃. Ощущается как ${Math.floor(
-        response.data.main.feels_like - 273.15
-      )} ℃. Скорость ветра: ${response.data.wind.speed} м/с. Влажность: ${
-        response.data.main.humidity
-      }%.`
-    );
+  if (text == "Покажи погоду") {
+    console.log(msg);
   } else {
     return bot.sendMessage(
       chatId,
       "Я не понимаю тебя. Попробуй ввести команду /info, она поможет ознакомиться с моими возможностями."
     );
   }
+});
+
+bot.on("callback_query", (msg) => {
+  const chatId = msg.message.chat.id;
+  console.log(msg);
+  bot.sendMessage(
+    chatId,
+    "Что бы узнать погоду вышлите мне геолокацию или нажмите  на кнопку ниже",
+    defaultKeyboard
+  );
 });
