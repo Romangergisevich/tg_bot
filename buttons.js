6af8bbe35f768d25cf9b723372054eb5a92099d8
unknown <Romangergisevich@gmail.com>
2024-01-12 19:28:57 +0300

    some weather features

--- buttons.js
+++ buttons.js
@@ -1,36 +1,32 @@
 module.exports = {
-  defaultKeyboard: {
+  weatherKeyboard: {
     reply_markup: JSON.stringify({
+      resize_keyboard: true,
       keyboard: [
         [
-          { text: "Покажи погоду", request_location: true, },
+          { text: "Погода в Бресте" },
+          { text: "Погода в геолокации", request_location: true },
         ],
       ],
-      resize_keyboard: true,
-      one_time_keyboard: true,
     }),
   },
 
   inlineKeyboardPanel: {
     reply_markup: JSON.stringify({
+      remove_keyboard: true,
       inline_keyboard: [
         [
-          { text: "погода", callback_data: "1" },
+          { text: "погода", callback_data: "weather_show" },
           { text: "2", callback_data: "2" },
           { text: "3", callback_data: "3" },
         ],
-        [
-          { text: "4", callback_data: "4" },
-          { text: "5", callback_data: "5" },
-          { text: "6", callback_data: "6" },
-        ],
-        [
-          { text: "7", callback_data: "7" },
-          { text: "8", callback_data: "8" },
-          { text: "9", callback_data: "9" },
-        ],
-        [{ text: "0", callback_data: "0" }],
       ],
+    }),
+  },
+
+  removeKeyboard: {
+    reply_markup: JSON.stringify({
+      remove_keyboard: true,
     }),
   },
 };
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
 const TelegramApi = require("node-telegram-bot-api");
 const tgToken = process.env.TG_TOKEN;
-const { defaultKeyboard, inlineKeyboardPanel } = require("./buttons");
+const {
+  inlineWeatherButton,
+  inlineKeyboardPanel,
+  weatherKeyboard,
+  removeKeyboard,
+} = require("./buttons");
 const axios = require("axios");
 const bot = new TelegramApi(tgToken, { polling: true });
+
+const weatherRequest = async (lat, long) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=b14e69a74786d8e6191dd31323bd1cc0`;
+  const response = await axios.get(url);
+  return response;
+};
 
 bot.setMyCommands([
   { command: "/start", description: "Начало работы" },
@@ -16,8 +27,7 @@ bot.on("message", async (msg) => {
   if (msg.location) {
     let latitude = msg.location.latitude;
     let longitude = msg.location.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=b14e69a74786d8e6191dd31323bd1cc0`;
-    const response = await axios.get(url);
+    const response = await weatherRequest(latitude, longitude);
     return bot.sendMessage(
       chatId,
       `На данный момент погода в ${
@@ -28,25 +38,37 @@ bot.on("message", async (msg) => {
         response.data.main.feels_like - 273.15
       )} ℃. Скорость ветра: ${response.data.wind.speed} м/с. Влажность: ${
         response.data.main.humidity
-      }%.`
+      }%.`, removeKeyboard
     );
   }
   if (text == "/start") {
     return bot.sendMessage(
       chatId,
-      `Привет, ${msg.from.first_name}!`,
-      inlineKeyboardPanel
+      `Привет, ${msg.from.first_name}! Чтобы ознакомиться с моими возможностями ты можешь ввести команду /info`
     );
   }
   if (text == "/info") {
     return bot.sendMessage(
       chatId,
-      `Hail to you, ${msg.from.first_name}. Что бы узнать погоду в своём местоположении, вышли свою геолокацию, либо введи команду /weather, тогда я выведу погоду в Бресте.`,
+      `Hail to you, ${msg.from.first_name}. На данный момент я могу вывести погоду в Бресте или в твоей геолокации, если твоё устройство позволяет передать геолокацию`,
       inlineKeyboardPanel
     );
   }
-  if (text == "Покажи погоду") {
-    console.log(msg);
+  if (text == "Погода в Бресте") {
+    const response = await weatherRequest(52.0975, 23.6877);
+    return bot.sendMessage(
+      chatId,
+      `На данный момент погода в ${
+        response.data.name
+      } выглядит так. Температура воздуха: ${Math.floor(
+        response.data.main.temp - 273.15
+      )} ℃. Ощущается как ${Math.floor(
+        response.data.main.feels_like - 273.15
+      )} ℃. Скорость ветра: ${response.data.wind.speed} м/с. Влажность: ${
+        response.data.main.humidity
+      }%.`,
+      removeKeyboard
+    );
   } else {
     return bot.sendMessage(
       chatId,
@@ -55,12 +77,14 @@ bot.on("message", async (msg) => {
   }
 });
 
-bot.on("callback_query", (msg) => {
+bot.on("callback_query", async (msg) => {
   const chatId = msg.message.chat.id;
   console.log(msg);
-  bot.sendMessage(
-    chatId,
-    "Что бы узнать погоду вышлите мне геолокацию или нажмите  на кнопку ниже",
-    defaultKeyboard
-  );
+  if (msg.data == "weather_show") {
+    return bot.sendMessage(
+      chatId,
+      "Что бы узнать погоду вышли мне геолокацию или нажми на одну из кнопок ниже",
+      (reply_markup = weatherKeyboard)
+    );
+  }
 });
